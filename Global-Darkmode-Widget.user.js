d4de3462df9da72b610237186ddcc4f9514e14a6
Marcel <suterma@users.noreply.github.com>
2020-06-26 22:16:56 +0200

    Update Global-Darkmode-Widget.user.js

--- Global-Darkmode-Widget.user.js
+++ Global-Darkmode-Widget.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name         Global Darkmode Widget
 // @namespace    darkmode-js
-// @version      0.1
+// @version      0.2
 // @description  Shows the Darkmode Widget all over the place
-// @author       qrys.ch
+// @author       marcel@qrys.ch
 // @include      *
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/darkmode-js@1.5.6/lib/darkmode-js.min.js
+// @updateURL    https://github.com/suterma/globaldarkmode/raw/master/Global-Darkmode-Widget.user.js
+// @downloadURL  https://github.com/suterma/globaldarkmode/raw/master/Global-Darkmode-Widget.user.js
+// @supportURL   https://github.com/suterma/globaldarkmode/issues
+
 // ==/UserScript==
 
 (function() {
